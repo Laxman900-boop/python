f2b9072dae1b6146bc9e58c23216d925684ece2d
Laxman900-boop <laxmankc525@gmail.com>
2025-03-27 09:16:56 +0000

    hello

--- hello3.py
+++ hello3.py
@@ -1,4 +1,4 @@
 dollor_rate = 0.93
 amount_usd = int(input("enter the amount:"))
 amount_euro = amount_usd * dollor_rate
-print(f"amuont_euro(euro)")
+print(f"{amount_usd} is equale to {amount_euro}")

1a262ffad5afd79169e33da4248a99e60289a2e8
Laxman900-boop <laxmankc525@gmail.com>
2025-03-27 09:58:02 +0000

    hello

---
+++ check_age.py
@@ -0,0 +1,6 @@
+user_name = input("enter your name")
+user_age = int(input("enter your :"))
+if user_age <18:
+    print("my game is not allowed under 18 years old")
+else:
+   print("welcome to my website and enjoy")

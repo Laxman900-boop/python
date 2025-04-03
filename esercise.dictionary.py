294889089a91ca79b31224014a82ada727cd732c
Laxman900-boop <laxmankc525@gmail.com>
2025-04-03 10:02:17 +0000

    print

---
+++ esercise.dictionary.py
@@ -0,0 +1,10 @@
+student = {
+"first_name": "Donald",
+"last_name": "Trump",
+"age": 54
+}
+
+
+print("first_name:", student["first_name"])
+print("last_name:", student["last_name"])
+print("age:", student["age"])

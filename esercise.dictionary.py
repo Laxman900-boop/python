43e392dba92f6dbc8f22ed5e3a2ca42ce2d167b3
Laxman900-boop <laxmankc525@gmail.com>
2025-04-03 10:09:04 +0000

    game editor

--- esercise.dictionary.py
+++ esercise.dictionary.py
@@ -1,10 +1,10 @@
-student = {
-"first_name": "Donald",
-"last_name": "Trump",
-"age": 54
+Game = {
+"name": "Cricket",
+"editor": ",Donald Trump",
+"year": 1954
 }
 
 
-print("first_name:", student["first_name"])
-print("last_name:", student["last_name"])
-print("age:", student["age"])
+print("name:", Game["name"])
+print("editor:", Game["editor"])
+print("year:", Game["year"])

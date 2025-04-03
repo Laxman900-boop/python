a0a1c260e4aec3d0c7be2915595cedb267a5e730
Laxman900-boop <laxmankc525@gmail.com>
2025-04-03 08:11:14 +0000

    serrl

--- exercise_list3.py
+++ exercise_list3.py
@@ -1,2 +1,20 @@
-Marks = []
-Total_marks = 0
+marks = []
+
+while True:
+    user_mark = input("Enter a mark(press enter to stop):\n")
+
+    if user_mark = "":
+         break
+    
+    try:
+         mark.append(float(user_mark))
+    except ValueError:
+         print("Invalid input. please enter a number or press enter to stop")
+
+         total = 0
+         #for mark in marks:
+           #   total += mark
+
+          #    average = total /len(marks)
+          
+              print(f"the average is: {round(average, 2)}")

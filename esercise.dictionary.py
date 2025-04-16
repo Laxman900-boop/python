f758bb374c41a55e6fd8a9504a138cf26110e0ec
Laxman900-boop <laxmankc525@gmail.com>
2025-04-16 10:14:53 +0000

    4

--- esercise.dictionary.py
+++ esercise.dictionary.py
@@ -1,10 +1,15 @@
-Game = {
-"name": "Cricket",
-"editor": ",Donald Trump",
-"year": 1954
+Movie = {
+"Name": "MUMMY-2",
+"Director": ",Donald Trump",
+"Actor": "K.P oli",
+"Year": 2025
+}
+ New_movie_name = {
+New_director : input("enter the name of movie:"),
+new_movie director : 
 }
 
-
-print("name:", Game["name"])
-print("editor:", Game["editor"])
-print("year:", Game["year"])
+print("Name:", Movie["Name"])
+print("Director:", Movie["Director"])
+print("Actor:", Movie["Actor"])
+print("Year:", Movie["Year"])       

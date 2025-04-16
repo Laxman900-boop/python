d9c3d30fdd75b8df0f8c9d0e59d78650636b10f1
Laxman900-boop <laxmankc525@gmail.com>
2025-04-16 10:11:12 +0000

    1

--- hello2.py
+++ hello2.py
@@ -1,4 +1,20 @@
-user_name = input("user_name")
-user_age = int("input(user_age)")
+def conversation():
+    print("Laxman: Hey Ram! How's it going?")
+    ram_response = input("Ram: I'm doing well, thanks! How about you? ")
 
-print("hello (user name), you are (user age) years old")
+    print("Laxman: I'm good too! Just enjoying the day.")
+    
+    favorite_color = input("Laxman: By the way, what's your favorite color? ")
+    print(f"Ram: Oh, I really like {favorite_color}! It's such a nice color.")
+
+    location = input("Laxman: Nice! Where do you live these days? ")
+    print(f"Ram: I live in {location}. It's a great place to be!")
+
+    hobbies = input("Laxman: What do you like to do in your free time? ")
+    print(f"Ram: I love {hobbies}. It really helps me unwind!")
+
+    print("Laxman: Sounds awesome! It was great catching up with you, Ram!")
+    print("Ram: Yeah, it was! Let's do this again soon.")
+
+# Call the function
+conversation()

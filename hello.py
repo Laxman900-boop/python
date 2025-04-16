235fc5123c2d9d746719a901a7c574e12a25db4a
Laxman900-boop <laxmankc525@gmail.com>
2025-04-16 10:17:28 +0000

    6

---
+++ hello.py
@@ -0,0 +1,6 @@
+user_name = input("what is your name")
+user_age = int(input("what is your user_age"))
+if user_age >=18:
+    print(my game is not allowed)
+else
+    print(welcome my website and enjoy)
---
+++ practice.py
@@ -0,0 +1,7 @@
+user_name = input("what is your name")
+user_number = float(input("what is your number"))
+user_age = int(input("what is your age"))
+if user_age < 23:
+    print("welcome to my website laxman")
+else:
+    print("you are not allowed")

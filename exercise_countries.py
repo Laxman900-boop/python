fd4c72e3c81619a34219c5d7408561056c360520
Laxman900-boop <laxmankc525@gmail.com>
2025-04-16 10:12:19 +0000

    2

---
+++ exercise_countries.py
@@ -0,0 +1,11 @@
+countries = [
+{"name": "China", "population": 34590998},
+{"name": "Nepal", "population": 36494890},
+{"name": "India", "population": 333333333333333},
+{"name": "france", "population": 44444444444444444444444},
+{"name": "Bangladesh", "population": 75555555544444444},
+{"name": "lanka", "population": 22222222222222222222222222222222222},
+
+print("List of countries:")
+
+    
